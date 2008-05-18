c9c02ef8f1da5b7a40851b9db3c6de2d25c80ff6
Christian Vest Hansen <karmazilla@gmail.com>
2008-05-18 17:14:26 +0200

    Removed the old, semaphore contended, nanopool implementation.

--- src/test/java/net/nanopool/PoolTestSupport.java
+++ src/test/java/net/nanopool/PoolTestSupport.java
@@ -2,12 +2,9 @@ package net.nanopool;
 
 import javax.sql.DataSource;
 
-import com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource;
-
 import edu.umd.cs.mtc.MultithreadedTest;
 
 public class PoolTestSupport extends MultithreadedTest {
-    private static final Log log = null;
     protected DataSource source;
     protected int totalSize;
     protected long timeout;
--- src/test/java/net/nanopool/PoolingDataSourceStateMixin.java
+++ src/test/java/net/nanopool/PoolingDataSourceStateMixin.java
@@ -2,17 +2,17 @@ package net.nanopool;
 
 import javax.sql.DataSource;
 
+import net.nanopool2.NanoPoolDataSource;
+
 import com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource;
 
 public class PoolingDataSourceStateMixin implements DataSourceState {
 	protected int totalSize;
 	protected long timeout;
-	protected Log log;
 
-	public PoolingDataSourceStateMixin(int totalSize, long timeout, Log log) {
+	public PoolingDataSourceStateMixin(int totalSize, long timeout) {
 		this.totalSize = totalSize;
 		this.timeout = timeout;
-		this.log = log;
 	}
 	
 	public DataSource createSource() {
@@ -21,7 +21,7 @@ public class PoolingDataSourceStateMixin implements DataSourceState {
 		cpds.setUser("root");
 		cpds.setPassword("");
 		cpds.setServerName("localhost");
-		return new PoolingDataSource(cpds, totalSize , timeout, log);
+		return new NanoPoolDataSource(cpds, totalSize , timeout);
 	}
 
 }

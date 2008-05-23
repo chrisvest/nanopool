5d1da40eda74f4ab58d0ec0834e27ff27575c7d3
Christian Vest Hansen <karmazilla@gmail.com>
2008-05-24 01:35:44 +0200

    Allow creating it to not be so talkative about contention.

--- src/main/java/net/nanopool/DefaultContentionHandler.java
+++ src/main/java/net/nanopool/DefaultContentionHandler.java
@@ -1,9 +1,19 @@
 package net.nanopool;
 
 public class DefaultContentionHandler implements ContentionHandler {
+    private final boolean printWarning;
+    
+    public DefaultContentionHandler() {
+        this(true);
+    }
+    
+    public DefaultContentionHandler(boolean printWarning) {
+        this.printWarning = printWarning;
+    }
     
     public void handleContention() {
-        System.err.println("PoolingDataSource: contention warning.");
+        if (printWarning)
+            System.err.println("PoolingDataSource: contention warning.");
         Thread.yield();
     }
 }

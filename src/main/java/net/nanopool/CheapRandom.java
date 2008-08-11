d5b5f8ab764c11f0eeb0b0d37435f6d76fac5f29
Christian Vest Hansen <karmazilla@gmail.com>
2008-08-11 21:53:21 +0200

    remove test/bench cruft from CheapRandom

--- src/main/java/net/nanopool/CheapRandom.java
+++ src/main/java/net/nanopool/CheapRandom.java
@@ -27,27 +27,4 @@ public final class CheapRandom {
         seed ^= (seed >>> 21);
         return seed ^ (seed << 7);
     }
-    
-    public static void main(String[] args) {
-        runTest();
-        runTest();
-        runTest();
-        runTest();
-        runTest();
-        runTest();
-        runTest();
-    }
-    
-    private static void runTest() {
-        long start = System.nanoTime();
-        CheapRandom rand = new CheapRandom();
-        // java.util.Random rand = new java.util.Random();
-        int r = 0;
-        for (int i = 0; i < 1000000; i++) {
-            r = rand.nextInt();
-        }
-        long end = System.nanoTime();
-        long elapsed = end - start;
-        System.out.printf("Time used: %s ms. r = %s\n", elapsed / 1000000.0, r);
-    }
 }

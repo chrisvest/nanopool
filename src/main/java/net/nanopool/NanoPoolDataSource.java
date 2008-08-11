c5291e67864cc71c4dcba99e378437a2ec0cc76e
Christian Vest Hansen <karmazilla@gmail.com>
2008-08-12 01:10:38 +0200

    javadoc for shutdown sequence and a package-info

--- src/main/java/net/nanopool/NanoPoolDataSource.java
+++ src/main/java/net/nanopool/NanoPoolDataSource.java
@@ -32,6 +32,28 @@ public final class NanoPoolDataSource extends PoolingDataSourceSupport {
                 poolSize, timeToLive, contentionHandler);
     }
     
+    /**
+     * Initiate a shutdown sequence on the pool.
+     * This method will return before the pool has completely shutdown, however
+     * the pool <strong>will</strong> be unable to grant any new connections.
+     * Calling {@link NanoPoolDataSource#getConnection()} on a shut down pool
+     * will result in an {@link IllegalStateException}.
+     * Calling {@link NanoPoolDataSource#shutdown()} on a pool that has already
+     * been shut down, has no effect.
+     * Connections that are active and in use when the pool is shut down will
+     * <strong>not</strong> be forcibly killed. Instead, all active connections
+     * will be allowed to operate for as long as they please, until they are
+     * closed normally or closed forcibly by the database or similar external
+     * factors. Any connections that are not in use, will be marked unavailable
+     * and closed. This behavior means that it is (relatively) safe to shut
+     * down a pool that is in use.
+     * Any {@link SQLException}s encountered during the shut down procedure
+     * are aggregated in a {@link List}, and that list is returned as the
+     * result of this method, for perusal by client code.
+     * @return A {@link List} of all {@link SQLException}s caught when shutting
+     * the pool down.
+     * @since 1.0
+     */
     public List<SQLException> shutdown() {
         return fsm.shutdown(connectors, poolSize);
     }
---
+++ src/main/java/net/nanopool/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Please see the {@link NanoPoolDataSource} documentation on
+ * general usage or this package - it's where the beef is.
+ */
+package net.nanopool;

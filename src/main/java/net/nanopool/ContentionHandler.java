132e776143c69391cad2812515355e621b783b74
Christian Vest Hansen <karmazilla@gmail.com>
2008-08-11 22:14:27 +0200

    javadoc

--- src/main/java/net/nanopool/ContentionHandler.java
+++ src/main/java/net/nanopool/ContentionHandler.java
@@ -1,5 +1,26 @@
 package net.nanopool;
 
+/**
+ * A ContentionHandler decides what to do when we reckon there's contention
+ * on the pool.
+ * Contention on the pool usually means that, last we checked, all connections
+ * was in use, so this particular can not get a connection right away. This
+ * means that we have wait a little while before we try again, and waiting
+ * is exactly what ContentionHandler implementations do.
+ * This interface is provided because the pool-using applications might want
+ * to do logging or other stuff, while they wait.
+ * The {@link NanoPoolDataSource} instances get their ContentionHandlers
+ * through their constructors when they are created.
+ * @author vest
+ *
+ */
 public interface ContentionHandler {
+    /**
+     * Do something to ease contention on the pool.
+     * This usually means calling {@link Thread#sleep(long)} or
+     * {@link Thread#yield()}, but you could also throw a form of
+     * {@link RuntimeException} if you'd rather give up on getting a connection
+     * for this particular thread - just be sure to catch it in your own code!
+     */
     void handleContention();
 }

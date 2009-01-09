1eb2541feeea24b4a047f21f45dc13ec8d20d4e7
Christian Vest Hansen <karmazilla@gmail.com>
2009-01-09 12:45:34 +0100

    A little more refactoring because we might want different nanopool "implementations" in the future.

---
+++ src/main/java/net/nanopool/AbstractDataSource.java
@@ -0,0 +1,48 @@
+package net.nanopool;
+
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author cvh
+ */
+public abstract class AbstractDataSource implements DataSource {
+    public PrintWriter getLogWriter() throws SQLException {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    public int getLoginTimeout() throws SQLException {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    public void setLogWriter(PrintWriter out) throws SQLException {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    public void setLoginTimeout(int seconds) throws SQLException {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    /**
+     * This method will always throw UnsupportedOperationException.
+     * @param <T>
+     * @param iface
+     * @return
+     * @throws java.sql.SQLException
+     */
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    /**
+     * This method will always throw UnsupportedOperationException.
+     * @param iface
+     * @return
+     * @throws java.sql.SQLException
+     */
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+}
--- src/main/java/net/nanopool/PoolingDataSourceSupport.java
+++ src/main/java/net/nanopool/PoolingDataSourceSupport.java
@@ -2,62 +2,43 @@ package net.nanopool;
 
 import java.io.PrintWriter;
 import java.sql.SQLException;
-
 import java.util.concurrent.locks.ReentrantLock;
 import javax.sql.ConnectionPoolDataSource;
-import javax.sql.DataSource;
 
 import net.nanopool.cas.CasArray;
 
-public abstract class PoolingDataSourceSupport implements DataSource {
-    final ReentrantLock resizingLock = new ReentrantLock();
+public abstract class PoolingDataSourceSupport extends AbstractDataSource {
     final ConnectionPoolDataSource source;
+    final ReentrantLock resizingLock = new ReentrantLock();
     final State state;
     volatile CasArray<Connector> connectors;
     volatile Connector[] allConnectors;
 
     PoolingDataSourceSupport(ConnectionPoolDataSource source,
             Configuration config) {
-        this.state = config.getState();
         this.source = source;
+        this.state = config.getState();
         this.connectors = state.buildCasArray();
         this.allConnectors = new Connector[connectors.length()];
     }
 
+    @Override
     public PrintWriter getLogWriter() throws SQLException {
         return source.getLogWriter();
     }
 
+    @Override
     public int getLoginTimeout() throws SQLException {
         return source.getLoginTimeout();
     }
 
+    @Override
     public void setLogWriter(PrintWriter out) throws SQLException {
         source.setLogWriter(out);
     }
 
+    @Override
     public void setLoginTimeout(int seconds) throws SQLException {
         source.setLoginTimeout(seconds);
-    }
-
-    /**
-     * This method will always throw UnsupportedOperationException.
-     * @param <T>
-     * @param iface
-     * @return
-     * @throws java.sql.SQLException
-     */
-    public <T> T unwrap(Class<T> iface) throws SQLException {
-        throw new UnsupportedOperationException("Not supported.");
-    }
-
-    /**
-     * This method will always throw UnsupportedOperationException.
-     * @param iface
-     * @return
-     * @throws java.sql.SQLException
-     */
-    public boolean isWrapperFor(Class<?> iface) throws SQLException {
-        throw new UnsupportedOperationException("Not supported.");
     }
 }

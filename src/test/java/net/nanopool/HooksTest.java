7a04afd32cca9dad7b7a997ad1075b7b40ad2d8a
Christian Vest Hansen <karmazilla@gmail.com>
2009-02-06 17:17:09 +0100

    added an unfinished hooks test

---
+++ src/test/java/net/nanopool/HooksTest.java
@@ -0,0 +1,54 @@
+package net.nanopool;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.concurrent.atomic.AtomicInteger;
+import javax.sql.ConnectionPoolDataSource;
+import net.nanopool.hooks.EventType;
+import net.nanopool.hooks.Hook;
+import org.junit.Test;
+
+/**
+ *
+ * @author cvh
+ */
+public class HooksTest extends NanoPoolTestBase {
+    private final AtomicInteger connectAttempts = new AtomicInteger();
+    private final AtomicInteger preConnectCheck = new AtomicInteger();
+    private final AtomicInteger postConnectCheck = new AtomicInteger();
+    private final AtomicInteger preReleaseCheck = new AtomicInteger();
+    private final AtomicInteger postReleaseCheck = new AtomicInteger();
+
+    static class SyncHook implements Hook {
+        private final String name;
+        private final AtomicInteger before;
+        private final AtomicInteger after;
+
+        SyncHook(String name, AtomicInteger before, AtomicInteger after) {
+            this.name = name;
+            this.before = before;
+            this.after = after;
+        }
+
+        public void run(EventType type, ConnectionPoolDataSource source, Connection con, SQLException sqle) {
+            assertEquals(name + " hook out of sync.",
+                    before.get(), after.incrementAndGet());
+        }
+    }
+
+    @Override
+    protected Configuration buildConfig() {
+        return super.buildConfig()
+                .addPreConnectHook(new SyncHook("pre connect", connectAttempts, preConnectCheck))
+                .addPostConnectHook(new SyncHook("post connect", preConnectCheck, postConnectCheck))
+                .addPreReleaseHook(new SyncHook("pre release", postConnectCheck, preReleaseCheck))
+                .addPostReleaseHook(new SyncHook("post release", preReleaseCheck, postReleaseCheck));
+    }
+
+    @Test
+    public void hooksMustRunInCorrectOrder() {
+
+    }
+}

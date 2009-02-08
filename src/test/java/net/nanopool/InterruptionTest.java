1b5a62269455613b961f703bc90ad08bb1dc6472
Christian Vest Hansen <karmazilla@gmail.com>
2009-02-08 17:10:31 +0100

    Working with the pool must not touch the Thread.interrupted state.
    Added a test to ensure this.

---
+++ src/test/java/net/nanopool/InterruptionTest.java
@@ -0,0 +1,26 @@
+package net.nanopool;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import org.junit.Test;
+
+/**
+ *
+ * @author cvh
+ */
+public class InterruptionTest extends NanoPoolTestBase {
+    @Test
+    public void mustPreserveInterruptionState() throws SQLException {
+        Thread.currentThread().interrupt();
+        pool = npds();
+        Connection con = pool.getConnection();
+        con.close();
+        List<SQLException> sqles = pool.shutdown();
+        for (SQLException sqle : sqles) sqle.printStackTrace();
+        assertTrue(sqles.isEmpty());
+        assertTrue(Thread.interrupted());
+    }
+}

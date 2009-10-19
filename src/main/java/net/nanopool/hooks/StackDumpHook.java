32a0ca469586b1e3262ecbdab98f834dbf7ba419
Christian Vest Hansen <karmazilla@gmail.com>
2009-10-19 19:31:52 +0200

    javadoc for the StackDumpHook

--- src/main/java/net/nanopool/hooks/StackDumpHook.java
+++ src/main/java/net/nanopool/hooks/StackDumpHook.java
@@ -20,16 +20,31 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
- * 
+ * The StackDumpHook prints a complete stack trace to a designated
+ * {@link PrintStream} every time it run. The stack traces are printed to
+ * {@link System#out} by default.
+ * <p>
+ * This hook is primarily for debugging purpose and you wouldn't want to use
+ * it in a production setting.
  * @author cvh
  */
 public class StackDumpHook implements Hook {
   private final PrintStream out;
   
+  /**
+   * Create a StackDumpHook that prints a complete stack trace to System.out
+   * when it runs.
+   */
   public StackDumpHook() {
     this(System.err);
   }
   
+  /**
+   * Create a StackDumpHook that prints a complete stack trace to the
+   * {@link PrintStream} specified as parameter.
+   * @param out The {@link PrintStream} to which the stack traces will be
+   * printed.
+   */
   public StackDumpHook(PrintStream out) {
     this.out = out;
   }

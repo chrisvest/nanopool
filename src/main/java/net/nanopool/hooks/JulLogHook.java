1e9c15a48c045ed7bad3852ebeed19077a4869ff
Christian Vest Hansen <karmazilla@gmail.com>
2009-10-19 19:24:09 +0200

    completed the javadoc for the JulLogHook

--- src/main/java/net/nanopool/hooks/JulLogHook.java
+++ src/main/java/net/nanopool/hooks/JulLogHook.java
@@ -21,39 +21,75 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * 
+ * The JulLogHook uses a {@link Logger} to log every time it is invoked.
+ * <p>
+ * The log level can be modified at run-time but the Logger is final.
  * @author cvh
  */
 public class JulLogHook implements Hook {
   private volatile Level logLevel;
   private final Logger logger;
   
+  /**
+   * Create a JulLogHook that uses the "net.nanopool" logger, and log
+   * {@link Level#INFO}.
+   */
+  public JulLogHook() {
+    this("net.nanopool");
+  }
+  
+  /**
+   * Create a JulLogHook that uses the logger with the specified name, and log
+   * {@link Level#INFO}.
+   * @param loggerName The name of the logger to use.
+   */
   public JulLogHook(String loggerName) {
     this(loggerName, Level.INFO);
   }
   
+  /**
+   * Create a JulLogHook that uses the logger with the specified name and
+   * log {@link Level}.
+   * @param loggerName The name of the {@link Logger} to use.
+   * @param level The {@link Level} to log with.
+   */
   public JulLogHook(String loggerName, Level level) {
     this(Logger.getLogger(loggerName), level);
   }
   
+  /**
+   * Create a JulLogHook with the specified {@link Logger} and {@link Level}.
+   * @param logger The {@link Logger} that this hook should log to.
+   * @param level The {@link Level} the messages should be logged as.
+   */
   public JulLogHook(Logger logger, Level level) {
     this.logger = logger;
     setLevel(level);
   }
   
+  /**
+   * Get the {@link Level} that is currently configured for this JulLogHook.
+   * @return A {@link Level} object - never null.
+   */
   public Level getLevel() {
     return logLevel;
   }
   
+  /**
+   * Set the {@link Level} that this JulLogHook should log messages at.
+   * @param level A non-null {@link Level} object.
+   */
   public void setLevel(Level level) {
-    if (level == null)
+    if (level == null) {
       throw new NullPointerException("Log level cannot be null.");
+    }
     logLevel = level;
   }
   
   public void run(EventType type, Connection con, SQLException sqle) {
-    if (!logger.isLoggable(logLevel))
+    if (!logger.isLoggable(logLevel)) {
       return;
+    }
     String msg = type.toString() + ": " + con;
     logger.log(logLevel, msg, sqle);
   }

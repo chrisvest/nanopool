83048acd3617acae2e3af200c4baf2c6765d7f35
Christian Vest Hansen <karmazilla@gmail.com>
2009-11-28 20:05:50 +0100

    fix the InterruptionTest that broke when we switched to embedded Derby by default in the tests. Who would have thought that Derby cleared the interruption flag?

--- src/test/java/net/nanopool/InterruptionTest.java
+++ src/test/java/net/nanopool/InterruptionTest.java
@@ -16,11 +16,21 @@
 package net.nanopool;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.sql.ConnectionEvent;
+import javax.sql.ConnectionEventListener;
+import javax.sql.ConnectionPoolDataSource;
+import javax.sql.PooledConnection;
+
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 /**
  * 
@@ -40,4 +50,41 @@ public class InterruptionTest extends NanoPoolTestBase {
     assertTrue(sqles.isEmpty());
     assertTrue(Thread.interrupted());
   }
+
+  @Override
+  protected ConnectionPoolDataSource buildCpds() throws SQLException {
+    final AtomicReference<ConnectionEventListener> connectionEventListener =
+      new AtomicReference<ConnectionEventListener>();
+    ConnectionPoolDataSource source = mock(ConnectionPoolDataSource.class);
+    final PooledConnection pcon = mock(PooledConnection.class);
+    Connection con = mock(Connection.class);
+    
+    when(source.getPooledConnection()).thenReturn(pcon);
+    doAnswer(new Answer<Object>() {
+      public Object answer(InvocationOnMock invocation) throws Throwable {
+        connectionEventListener.set(
+            (ConnectionEventListener) invocation.getArguments()[0]);
+        return null;
+      }
+    }).when(pcon).addConnectionEventListener(
+        (ConnectionEventListener) anyObject());
+    doAnswer(new Answer<Object>() {
+      public Object answer(InvocationOnMock invocation) throws Throwable {
+        ConnectionEventListener cel = connectionEventListener.get();
+        ConnectionEvent event = new ConnectionEvent(pcon);
+        cel.connectionClosed(event);
+        return null;
+      }
+    }).when(con).close();
+    
+    when(pcon.getConnection()).thenReturn(con);
+    
+    return source;
+  }
 }
+
+
+
+
+
+
